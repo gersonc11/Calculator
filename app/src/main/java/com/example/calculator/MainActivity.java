601c309895e58288ba6f6d083d59fe212b52e17b
gersonc11 <gcruz1776@gmail.com>
2019-07-02 22:58:16 -0400

    Added Butterknife library to MainActivity Java class

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,176 +1,89 @@
 package com.example.calculator;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnPlus, btnMinus, btnDivide, btnPercent, btnClear, btnNegative, btnMultiply, btnDot;
+    @BindView(R.id.textSpace)
     TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        btn1 = findViewById(R.id.one);
-        btn2 = findViewById(R.id.two);
-        btn3 = findViewById(R.id.three);
-        btn4 = findViewById(R.id.four);
-        btn5 = findViewById(R.id.five);
-        btn6 = findViewById(R.id.six);
-        btn7 = findViewById(R.id.seven);
-        btn8 = findViewById(R.id.eight);
-        btn9 = findViewById(R.id.nine);
-        btn0 = findViewById(R.id.zero);
-        btnPlus = findViewById(R.id.plus);
-        btnMinus = findViewById(R.id.minus);
-        btnMultiply = findViewById(R.id.times);
-        btnDivide = findViewById(R.id.divide);
-        btnPercent = findViewById(R.id.percent);
-        btnClear = findViewById(R.id.cButton);
-        btnDot = findViewById(R.id.dot);
-        btnNegative = findViewById(R.id.slash);
-        display = findViewById(R.id.textSpace);
+    }
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @OnClick({R.id.one, R.id.two, R.id.three, R.id.four, R.id.five, R.id.six, R.id.seven, R.id.eight, R.id.nine, R.id.zero, R.id.plus, R.id.minus, R.id.times, R.id.divide, R.id.equals, R.id.slash, R.id.cButton, R.id.dot, R.id.percent})
+    public void pressingButtons(Button view) {
+        switch (view.getId()) {
+            case R.id.one:
                 display.append("1");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.two:
                 display.append("2");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.three:
                 display.append("3");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.four:
                 display.append("4");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.five:
                 display.append("5");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.six:
                 display.append("6");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.seven:
                 display.append("7");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.eight:
                 display.append("8");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.nine:
                 display.append("9");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.zero:
                 display.append("0");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnNegative.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                display.append("+/-");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.plus:
                 display.append("+");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.minus:
                 display.append("-");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.times:
                 display.append("*");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.divide:
                 display.append("/");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                display.append("C");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.cButton:
+                display.setText("");
+                break;
+            case R.id.equals:
+                display.append("=");
+                break;
+            case R.id.dot:
                 display.append(".");
-                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-            }
-        });
+                break;
+            case R.id.slash:
+                display.append("+/-");
+                break;
+            case R.id.percent:
+                display.append("%");
+                break;
+        }
     }
 
     @Override
